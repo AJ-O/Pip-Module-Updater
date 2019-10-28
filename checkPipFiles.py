ffbdd989147840a25de7ebbb9bfcab25988cecf4
AJ-O <ashishleiot@gmail.com>
2019-10-29 00:01:23 +0530

    Comments and quit feature

--- checkPipFiles.py
+++ checkPipFiles.py
@@ -3,6 +3,7 @@ import json
 
 from selenium import webdriver
 
+#get the current pip modules and save it to a dictionary
 def getData():
 
     var = os.popen('pip3 list --format=json').read()
@@ -18,25 +19,26 @@ def getData():
     #print(valdict.values())
     return valdict
 
+#Using selenium to extract the latest pip modules as the user has
 def moduleCheck():
 
     data = getData()
-    newVersions = []
-    olderVersions = []
-    moduleNames = []
-    elements = []
-    moduleInfo = []
+    newVersions = []#store the new versions of the modules
+    olderVersions = []#store the older versions for comparision
+    moduleNames = []#store the pip modules
+    elements = []#storing the elements extracted via selenium
+    moduleInfo = []#store olderVersions, newVersions, moduleNames as n*3 matrix
 
     baseUrl = "https://pypi.org/project/"
     driver = webdriver.Firefox('./')
-    modulesNotFound = []
+    modulesNotFound = []#to remove those modules that don't exist or support is removed
 
     for mname in data.keys():
 
         try:
 
             driver.get(baseUrl + mname)
-            nameTag = driver.find_element_by_class_name('package-header__name')
+            nameTag = driver.find_element_by_class_name('package-header__name')#this element contains the module name and latest version
             elements.append(nameTag.text)
 
         except:
@@ -46,19 +48,20 @@ def moduleCheck():
 
     driver.quit()
 
+    #remove the modules that are discontinued
     for i in range(len(modulesNotFound)):
 
         del data[modulesNotFound[i]]
 
-    i = 0
+    i = 0#to iterate through element list which contain the html tags
 
     for mname in data.keys():
 
-        temp = elements[i].split(' ')
+        temp = elements[i].split(' ')#split the tag as the first part contains the module name, second contains the version
 
         #print('system m: {0} system v: {1} installed version: {2}'.format(mname, data[mname], temp[1]) )
 
-        if temp[1] != data[mname]:
+        if temp[1] != data[mname]:#if the versions vary only then continue
 
             newVersions.append(temp[1])
             olderVersions.append(data[mname])
--- installGui.py
+++ installGui.py
@@ -21,7 +21,7 @@ class App(QWidget):
         self.height = 300
         self.indices = []
         self.chkBoxItem = []
-        self.data = moduleCheck()
+        self.data = moduleCheck()#getting data from the checkPipFiles file
         self.initUI()
 
     def initUI(self):
@@ -29,13 +29,13 @@ class App(QWidget):
         self.setWindowTitle(self.title)
         self.setGeometry(self.left, self.top, self.width, self.height)
 
-        self.createTable()
+        self.createTable()#to create a table that will store the module name, older version, new version and a check box to download or not
 
-        self.layout = QVBoxLayout()
+        self.layout = QVBoxLayout()#table is added into it
         self.layout.addWidget(self.tableWidget)
         self.setLayout(self.layout)
 
-        button = QPushButton('All selected', self)
+        button = QPushButton('All selected', self)#when the user is finished selecting the modules they want to update
         button.move(450, 150)
         button.clicked.connect(self.on_done)
         self.show()
@@ -48,35 +48,37 @@ class App(QWidget):
         self.tableWidget.setColumnCount(4)
         self.tableWidget.setHorizontalHeaderLabels(["Module Name", "Old Version", "New Version", "Download?"])
 
+        #setting the table with data received via moduleCheck function
         for i in range(len(self.data[0])):
 
             self.tableWidget.setItem(i, 0, QTableWidgetItem(self.data[0][i]))
             self.tableWidget.setItem(i, 1, QTableWidgetItem(self.data[1][i]))
             self.tableWidget.setItem(i, 2, QTableWidgetItem(self.data[2][i]))
 
+            #Creating a checkbox of type QTableWidgetItem
             self.chkBoxItem.append(QTableWidgetItem())
             self.chkBoxItem[i].setFlags(Qt.ItemIsUserCheckable | Qt.ItemIsEnabled)
             self.chkBoxItem[i].setCheckState(Qt.Unchecked)
             self.tableWidget.setItem(i, 3, self.chkBoxItem[i])
 
+    #function to check those modules that the user has selected that they want to update
     def on_done(self):
 
         for i in range(len(self.chkBoxItem)):
 
             val = self.chkBoxItem[i].checkState()
 
-            if val == 2:
+            if val == 2:#if checkbox is checked
 
                 self.indices.append(i)
 
-        print(self.indices)
-
         for i in self.indices:
 
-            print("selected: ", self.data[0][i])
+            print("selected modules to update: ", self.data[0][i])
 
         self.installModules(self.indices)
 
+    #Update the selected modules
     def installModules(self, indices):
 
         for i in indices:
